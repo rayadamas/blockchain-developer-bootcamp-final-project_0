c032066276846211a55134674f99da9f2b6cb9e6
Ray Adamas <djadamasray@gmail.com>
2021-11-26 19:14:09 -0600

    add comment

--- sardaukarchant - Copy/scripts/run.js
+++ sardaukarchant - Copy/scripts/run.js
@@ -1,10 +1,20 @@
 const main = async () => {
+ // waveContractFactory compiles contract + generates files needed in `./artifacts` 
  const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+ /*
+  * Hardhat creates a local ETH network for you, JUST for THIS contract. After script completion it will DESTRUCT
+  This process can be repeated.
+  `hre` & `hre.ethers` (Hardhat Runtime Environment) is an onj containing all the functionality that Hardhat
+  exposes when running a task, test, or scrpit. In reality, Hardhart is HRE.
+  */
  const waveContract = await waveContractFactory.deploy({
+   // `value` instructs this contract be deployed w an inital funding of 0.1 ethers, extracting from users wallet
    value: hre.ethers.utils.parseEther('0.1'),
  });
+ // We patiently wait until the contract is deployed locally
  await waveContract.deployed();
- console.log('Contract addy:', waveContract.address);
+ // How we are able to locate our contract on the blockchain
+ console.log('Address of Contract:', waveContract.address);
 
  let contractBalance = await hre.ethers.provider.getBalance(
    waveContract.address
@@ -14,18 +24,19 @@ const main = async () => {
    hre.ethers.utils.formatEther(contractBalance)
  );
 
+ 
  /*
-  * Let's try two waves now
-  */
- const waveTxn = await waveContract.wave('This is wave #1');
+ * Additional CHANTS commented out. Adjust cooldown time to test.
+ */
+ const waveTxn = await waveContract.wave('This is the first CHANT');
  await waveTxn.wait();
 
- const waveTxn2 = await waveContract.wave('This is wave #2');
- await waveTxn2.wait();
-
+  //  const waveTxn2 = await waveContract.wave('This is wave #2');
+  //  await waveTxn2.wait();
+  // We see what has happened to our balance after deploying/CHANTING
  contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
  console.log(
-   'Contract balance:',
+   'Contract funding:',
    hre.ethers.utils.formatEther(contractBalance)
  );
 
