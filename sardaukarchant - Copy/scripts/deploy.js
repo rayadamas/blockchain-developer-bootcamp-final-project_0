a74bb3df4c4840e0b7b5ffec14ebd88aaac82fad
Ray Adamas <djadamasray@gmail.com>
2021-11-26 18:56:52 -0600

    add comment

--- sardaukarchant - Copy/scripts/deploy.js
+++ sardaukarchant - Copy/scripts/deploy.js
@@ -1,6 +1,8 @@
 const main = async () => {
  const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
  const waveContract = await waveContractFactory.deploy({
+   // `value` instructs this contract be deployed w an inital funding of 0.001 ethers, extracting from users wallet
+   // this is a minimal amount as greater expenses would be wasted
    value: hre.ethers.utils.parseEther('0.001'),
  });
 
